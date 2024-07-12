400d956c1f524c32e83d00c63be6617431e4da53
TansuAlt <tansualtin3@gmail.com>
2024-07-12 16:49:04 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // MySQL bağlantısı sağlanır
+            String jdbcUrl = "jdbc:mysql://localhost:3306/new_schema?useSSL=false";
+            String username = "root";
+            String password = "tansu";
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // XML dosyasını parse etme
+            File xmlFile = new File("C:\\Users\\Tansu\\IdeaProjects\\PROJE\\src\\operators.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+
+            // Kök elementten Company düğümlerini alınır
+            NodeList companyList = doc.getElementsByTagName("Company");
+
+            // Her Company düğümünü istediğimiz company düğümü ise döndürür
+            for (int i = 0; i < companyList.getLength(); i++) {
+                Element company = (Element) companyList.item(i);
+
+                // CompanyName ve Route değerlerini alınır
+                String companyName = company.getElementsByTagName("CompanyName").item(0).getTextContent();
+                String route = company.getElementsByTagName("Route").item(0).getTextContent();
+
+                // Block düğümlerini alınır
+                NodeList blockList = company.getElementsByTagName("Block");
+
+                // Her Block düğümünü döndürür
+                for (int j = 0; j < blockList.getLength(); j++) {
+                    Element block = (Element) blockList.item(j);
+
+                    // NDC, Prefix ve Size yazdırır
+                    String ndc = block.getElementsByTagName("NDC").item(0).getTextContent();
+                    String prefix = block.getElementsByTagName("Prefix").item(0).getTextContent();
+                    String size = block.getElementsByTagName("Size").item(0).getTextContent();
+
+                    // Veritabanına ekleme
+                    insertIntoDatabase(connection, companyName, route, ndc, prefix, size);
+                }
+            }
+
+
+            connection.close();
+
+        } catch (ParserConfigurationException | IOException | SAXException | SQLException e) {
+            // Hata durumunda loglama yap e.printStackTrace() uyarı verdiği için eklenildi
+            System.err.println("Veritabanına veri eklenirken hata oluştu:");
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertIntoDatabase(Connection connection, String companyName, String route, String ndc, String prefix, String size) throws SQLException {
+        // mysql için INSERT sorgusu
+        String sql = "INSERT INTO new_schema.prefix_tanimlari (operator, prefix, operator_kodu) VALUES (?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, companyName); // operator sütunu için companyName kullanıldı
+        statement.setString(2, prefix);
+        statement.setInt(3, Integer.parseInt(ndc));
+
+        // Sorguyu çalıştır
+        statement.executeUpdate();
+        statement.close();
+    }
+}
